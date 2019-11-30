c703b6c01e3ceb36348775076000bf9eda28b9dd
rurimo <topmazino@gmail.com>
2019-11-30 12:28:01 +0900

    Refactor and rearrange codes

--- android/app/src/main/java/com/skydoves/planes/MainActivity.kt
+++ android/app/src/main/java/com/skydoves/planes/MainActivity.kt
@@ -3,47 +3,50 @@ package com.skydoves.planes
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.KeyEvent.ACTION_DOWN
-import android.view.KeyEvent.ACTION_UP
-import android.view.KeyEvent.KEYCODE_DPAD_DOWN
-import android.view.KeyEvent.KEYCODE_DPAD_LEFT
-import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
-import android.view.KeyEvent.KEYCODE_DPAD_UP
-import android.view.KeyEvent.KEYCODE_W
+import android.view.KeyEvent.*
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.button_back
-import kotlinx.android.synthetic.main.activity_main.button_go
-import kotlinx.android.synthetic.main.activity_main.button_left
-import kotlinx.android.synthetic.main.activity_main.button_right
-import kotlinx.android.synthetic.main.activity_main.button_stop
-import kotlinx.android.synthetic.main.activity_main.webView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
+  // composite all disposable observables
   private val compositeDisposable = CompositeDisposable()
 
-  @SuppressLint("SetJavaScriptEnabled")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    // initialize webView settings
+    initializeWebView()
+    // initialize interval emmit resource from observable
+    initializeIntervalObservable()
+    // initialize UI things
+    initializeUI()
+  }
+
+  @SuppressLint("SetJavaScriptEnabled")
+  private fun initializeWebView() {
     webView.settings.javaScriptEnabled = true
     webView.loadUrl("file:///android_asset/plane/index.html")
     webView.webViewClient = object : WebViewClient() {
     }
+  }
 
+  private fun initializeIntervalObservable() {
     val observable = Observable.interval(200L, TimeUnit.MILLISECONDS).timeInterval()
       .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe {
         webView.dispatchKeyEvent(KeyEvent(ACTION_DOWN, KEYCODE_W))
       }
     this.compositeDisposable.add(observable)
+  }
 
+  private fun initializeUI() {
     button_go.setOnClickListener {
       actionUpAllKeys()
       webView.dispatchKeyEvent(KeyEvent(ACTION_DOWN, KEYCODE_DPAD_DOWN))
