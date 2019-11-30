ea8fe790c0ac27a0d7a19923c033b4fbc457d7a7
rurimo <topmazino@gmail.com>
2019-11-30 12:45:39 +0900

    Implement go moves key

--- android/app/src/main/java/com/skydoves/planes/MainActivity.kt
+++ android/app/src/main/java/com/skydoves/planes/MainActivity.kt
@@ -3,14 +3,30 @@ package com.skydoves.planes
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.KeyEvent.*
+import android.view.KeyEvent.ACTION_DOWN
+import android.view.KeyEvent.ACTION_UP
+import android.view.KeyEvent.KEYCODE_DPAD_DOWN
+import android.view.KeyEvent.KEYCODE_DPAD_LEFT
+import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
+import android.view.KeyEvent.KEYCODE_DPAD_UP
+import android.view.KeyEvent.KEYCODE_E
+import android.view.KeyEvent.KEYCODE_Q
+import android.view.KeyEvent.KEYCODE_W
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.button_back
+import kotlinx.android.synthetic.main.activity_main.button_go
+import kotlinx.android.synthetic.main.activity_main.button_left
+import kotlinx.android.synthetic.main.activity_main.button_right
+import kotlinx.android.synthetic.main.activity_main.button_stop
+import kotlinx.android.synthetic.main.activity_main.button_turn_left
+import kotlinx.android.synthetic.main.activity_main.button_turn_right
+import kotlinx.android.synthetic.main.activity_main.button_up
+import kotlinx.android.synthetic.main.activity_main.webView
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -66,6 +82,9 @@ class MainActivity : AppCompatActivity() {
       actionUpAllTurnKeys()
       webView.dispatchKeyEvent(KeyEvent(ACTION_DOWN, KEYCODE_DPAD_UP))
     }
+    button_go.setOnClickListener {
+      actionUpAllMoveKeys()
+    }
     button_left.setOnClickListener {
       actionUpAllMoveKeys()
       webView.dispatchKeyEvent(KeyEvent(ACTION_DOWN, KEYCODE_Q))
@@ -76,6 +95,7 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  /** invokes action up all keys related to turn the plane. */
   private fun actionUpAllTurnKeys() {
     webView.dispatchKeyEvent(KeyEvent(ACTION_UP, KEYCODE_DPAD_UP))
     webView.dispatchKeyEvent(KeyEvent(ACTION_UP, KEYCODE_DPAD_LEFT))
@@ -83,11 +103,13 @@ class MainActivity : AppCompatActivity() {
     webView.dispatchKeyEvent(KeyEvent(ACTION_UP, KEYCODE_DPAD_DOWN))
   }
 
+  /** invokes action up all keys related to move the plane. */
   private fun actionUpAllMoveKeys() {
     webView.dispatchKeyEvent(KeyEvent(ACTION_UP, KEYCODE_Q))
     webView.dispatchKeyEvent(KeyEvent(ACTION_UP, KEYCODE_E))
   }
 
+  /** destroys all disposables. */
   override fun onDestroy() {
     this.compositeDisposable.dispose()
     super.onDestroy()
